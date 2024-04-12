f8bfc0c2a3c16fcebd8fdba5fcd912876b1efcf6
Ashab-al <asxabal7@gmail.com>
2024-04-12 20:59:22 +0300

    write tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do 
+  describe 'validations' do 
+    it 'valid enum status' do 
+      expect(User.new(status: nil)).to_not be_valid
+      expect(User.new(status: :author)).to be_valid
+      expect(User.new(status: :guest)).to be_valid
+      expect(User.new(status: :admin)).to_not be_valid
+    end
+  end
 end
