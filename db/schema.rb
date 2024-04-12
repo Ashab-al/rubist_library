ea64747d9a034f49119311cfd0a9f4618966149e
Ashab-al <asxabal7@gmail.com>
2024-04-12 15:36:50 +0300

    create model chapter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_23_141232) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_12_121338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_141232) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
+  create_table "chapters", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_chapters_on_book_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "last_name"
@@ -33,4 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_141232) do
   end
 
   add_foreign_key "books", "users"
+  add_foreign_key "chapters", "books"
 end
