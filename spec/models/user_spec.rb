73ceefea01758f5431d9d93779c6cc8011b085a4
Ashab-al <asxabal7@gmail.com>
2024-04-13 18:48:05 +0300

    edited user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do 
   describe 'validations' do 
     it 'valid enum status' do 
-      expect(User.new(status: nil)).to_not be_valid
       expect(User.new(status: :author)).to be_valid
       expect(User.new(status: :guest)).to be_valid
+    end
+
+    it 'user has status nil' do 
+      expect(User.new(status: nil)).to_not be_valid
+    end
+    
+    it 'user has status admin' do 
       expect(User.new(status: :admin)).to_not be_valid
     end
   end
